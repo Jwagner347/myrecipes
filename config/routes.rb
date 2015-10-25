cd307fadacf551eb5cb4fbadf58b6d241e294832
Jeff Wagner <jwagner@thoughtworks.com>
2015-10-25 15:29:11 -0700

    create chefs resources and add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,18 @@ Rails.application.routes.draw do
       post 'like'
     end
   end
-  #this is doing this:
-  # get '/recipes', to: 'recipes#index'
-  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  # post '/recipes', to: 'recipes#create'
-  # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
-  # patch '/recipes/:id', to: 'recipes#update'
-  # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
-  # delete '/recipes/:id', to: 'recipes#destroy'
+
+  resources :chefs, except: [:new]
+
+  get '/register', to: 'chefs#new'
+
+  get '/login', to: 'logins#new'
+  post '/login', to: 'logins#create'
+  get '/logout', to: 'logins#destroy'
+
+
+
+
 
 
 end
